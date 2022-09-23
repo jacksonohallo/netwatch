0923b2a6781f2ec15b1a60f92698053a9bf0a7d8
jacksonohallo <jacksonohallo@gmail.com>
2022-09-23 10:04:46 +0300

    add movies release date

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,15 +23,18 @@ const Main = () => {
           alt={movie?.title}
         />
               <div className='absolute w-full top-[20%] p-4 md:p-8'>
-                  <h1>{movie?.title}</h1>
-          <div>
+                  <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+          <div className='my-4'>
             <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
               Play
             </button>
             <button className="border text-white border-gray-300 py-2 px-5 ml-4">
               Watch Later
             </button>
-          </div>
+                  </div>
+                  <p className='text-gray-400 text-sm'> 
+                 Released: {movie?.release_date}
+                  </p>
         </div>
       </div>
     </div>

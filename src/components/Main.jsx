6bacadd9f3df5db654d966495f872a11505c3291
jacksonohallo <jacksonohallo@gmail.com>
2022-09-23 09:39:48 +0300

    display movies on the homepage from api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,26 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import requests from './Requests'
-
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import requests from "./Requests";
 
 const Main = () => {
-    const [movies, setMovies] = useState([])
-    const movie = movies[Math.floor(Math.random() * movies.length)]
-    
-    useEffect(() => {
+  const [movies, setMovies] = useState([]);
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+  useEffect(() => {
+    axios.get(requests.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
 
-        axios.get(requests.requestPopular).then((response) => {
-            
-            setMovies(response.data)
-        }
+  console.log(movie);
 
-        )
-        
-    }, [])
-    
-    console.log(movie)
-  
-  
-    return (
-    <div>Main</div>
-  )
-}
+  return (
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+      <img  src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+        alt={movie?.title} />
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ import Main from '../Main'
 const Home = () => {
   return (
       <div>
-          <Main  />
+        <Main/>
     </div>
   )
 }

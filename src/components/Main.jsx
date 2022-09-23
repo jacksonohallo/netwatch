b959f398ff2913996304794b475cb8cf57437936
jacksonohallo <jacksonohallo@gmail.com>
2022-09-23 09:21:11 +0300

    fucntion that enable random selection of one movie

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import requests from './Requests'
 
 
 
 const Main = () => {
-  const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([])
+    const movie = movies[Math.floor(Math.random() * movies.length)]
+    
+    useEffect(() => {
+
+        axios.get(requests.requestPopular).then((response) => {
+            
+            setMovies(response.data)
+        }
+
+        )
+        
+    }, [])
+    
+    console.log(movie)
   
   
     return (
